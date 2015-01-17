234d849b6473b3b0d2aa89c56d77439ffa135c1b
thibaultCha <thibaultcha@me.com>
2015-01-16 20:00:17 -0800

    gulp-uncss + sourcemaps path fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@
 var del = require('del');
 var gulp = require('gulp');
 var less = require('gulp-less');
+var uncss = require('gulp-uncss');
 var combiner = require('stream-combiner2');
 var sourcemaps = require('gulp-sourcemaps');
 var less_clean = require('less-plugin-clean-css');
 var less_prefix = require('less-plugin-autoprefix');
 
+var uncss = uncss({ html: ['index.html', 'docs.html'] });
 var clean = new less_clean({ advanced: true });
 var prefix = new less_prefix({ browsers: ['last 2 versions'] });
 
@@ -32,7 +34,8 @@ gulp.task('build', function () {
     gulp.src(src.less),
     sourcemaps.init(),
     less({ plugins: [clean, prefix] }),
-    sourcemaps.write(dist.css + '/maps'),
+    uncss,
+    sourcemaps.write('maps'),
     gulp.dest(dist.css)
   ]).on('error', console.error.bind(console));
 });
@@ -53,6 +56,7 @@ gulp.task('dist', ['clean'], function () {
   return combiner.obj([
     gulp.src(src.less),
     less({ plugins: [clean, prefix] }),
+    uncss,
     gulp.dest(dist.css)
     ]).on('error', console.error.bind(console));
 });
